12fbdde5b3ab04f7fe9efffc981379a497c679c9
MattDSquared <matthew.d.davis@gmail.com>
2015-09-25 15:22:08 -0700

    converted to ggplot2 for pretty graphics

--- server.R
+++ server.R
@@ -1,27 +1,36 @@
 library(datasets); data(mtcars)
+library(ggplot2)
 
 shinyServer(
     function(input, output) {
         output$myScatter <- renderPlot({
-            plot(mtcars$am, mtcars$mpg, 
-                 xlab="Transmission type", 
-                 ylab="Miles Per Gallon", 
-                 col="lightblue",
-                 main="Find the slope and intercept for a best linear fit through the data.")
+            gg <- ggplot(mtcars, aes(x=am, y=mpg))
+            gg <- gg + geom_point(colour="steelblue") +
+                labs(x="Transmission type") +
+                labs(y="Miles Per Gallon") + 
+                labs(title="Find the slope and intercept for a best linear fit through the data.")
             alpha <- input$alpha
             beta <- input$beta
-            abline(alpha, beta, col="red", lwd=3)
-            mse <- mean((mtcars$mpg - (alpha + beta*mtcars$am))^2)
+            gg <- gg + geom_abline(intercept = alpha, slope = beta, 
+                                   colour = "red", size = 1.5)
+            print(gg)
+        })
+        
+        fitDataValues <- reactive({
+            mse <- mean((mtcars$mpg - (input$alpha + input$beta*mtcars$am))^2)
             
-            textpos.x <- .25*max(mtcars$am)
-            textpos.y <- .9*max(mtcars$mpg)
-            textpos.linewidth <- diff(range(mtcars$mpg))/20
-            text(textpos.x, textpos.y+2*textpos.linewidth, 
-                 paste("intercept = ", alpha))
-            text(textpos.x, textpos.y+textpos.linewidth, 
-                 paste("slope = ", beta))
-            text(textpos.x, textpos.y, 
-                 paste("MSE = ", round(mse, 2)))
+            data.frame(
+                Name = c("Intercept", 
+                         "Slope",
+                         "Mean Squraed Error (MSE)"),
+                Value = as.character(c(input$alpha, 
+                                       input$beta,
+                                       mse)), 
+                stringsAsFactors=FALSE)
+        })
+        
+        output$fitData <- renderTable({
+            fitDataValues()
         })
         
     }
--- ui.R
+++ ui.R
@@ -1,14 +1,18 @@
 library(shiny)
 
 shinyUI(pageWithSidebar(
+    
     headerPanel("Beat the computer: Find the best fit line."),
+    
     sidebarPanel(
         sliderInput('alpha', 'Guess at the intercept, alpha',
-                    value = 10, min = 0, max = 50, step = .5),
+                    value = 30, min = 0, max = 50, step = .5),
         sliderInput('beta', 'Guess at the slope, beta',
                     value = 1, min = -10, max = 10, step = 0.1)
     ),
+    
     mainPanel(
-        plotOutput('myScatter')
+        plotOutput('myScatter'), 
+        tableOutput("fitData")
     )
 ))
