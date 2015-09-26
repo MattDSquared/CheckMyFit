e2f5c3e02e6667fa48d9edb34eff4d90c524dfa9
MattDSquared <matthewddavis@gmail.com>
2015-09-26 14:07:58 -0700

    Revert "Changed to numericInput for better UI"
    This reverts commit a2563969a1df679b7b63d61581d52803f2a853e8.
    
    reverted test commit

--- ui.R
+++ ui.R
@@ -5,10 +5,10 @@ shinyUI(pageWithSidebar(
     headerPanel("Beat the computer: Find the best fit line."),
     
     sidebarPanel(
-        numericInput('alpha', 'Guess at the intercept [mpg]',
-                    value = 25, min = 0, max = 100, step = .5),
-        numericInput('beta', 'Guess at the slope, [mpg/x-var]',
-                    value = 0, min = -20, max = 20, step = 0.1)
+        sliderInput('alpha', 'Guess at the intercept, alpha',
+                    value = 30, min = 0, max = 50, step = .5),
+        sliderInput('beta', 'Guess at the slope, beta',
+                    value = 1, min = -10, max = 10, step = 0.1)
     ),
     
     mainPanel(
